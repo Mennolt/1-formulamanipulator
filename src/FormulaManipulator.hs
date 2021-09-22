05d7596700aa8b6f3d53cea13aa5482c3c292a30
Mennolt van Alten <mennolt.alten@student.tue.nl>
2021-09-22 12:52:24 +0200

    Functionality evalE for constants

--- src/FormulaManipulator.hs
+++ src/FormulaManipulator.hs
@@ -25,7 +25,7 @@ import           ExprLanguage                   ( Expr(Var, Const, Plus, Mult) )
   -stepMul for the function to apply when reaching a multiply
   -}
 
-foldE :: (Const -> a) -> (Var -> a) -> (a -> a -> a) -> (a -> a -> a) -> Expr b c -> a
+foldE :: (c -> a) -> (b -> a) -> (a -> a -> a) -> (a -> a -> a) -> Expr b c -> a
 foldE baseConst baseVar stepPlus stepMult = rec
             where
              rec  (Const i) = baseConst i
@@ -33,7 +33,23 @@ foldE baseConst baseVar stepPlus stepMult = rec
              rec  (Plus eq1 eq2) = stepPlus (rec eq1) (rec eq2) 
              rec  (Mult eq1 eq2) = stepMult (rec eq1) (rec eq2)
 
+
+
 printE    = error "Implement, document, and test this function"
-evalE     = error "Implement, document, and test this function"
+
+--evalE :: Expr b a -> a
+evalE     = foldE evalConst evalVar evalPlus evalMult --error "Implement, document, and test this function"
+
+evalConst :: a -> a
+evalConst = id
+
+evalVar lookup = lookup--to be improved
+
+evalPlus :: (Num a) => a -> a -> a
+evalPlus = (+) 
+
+evalMult :: (Num a) => a -> a -> a
+evalMult = (*)
+
 simplifyE = error "Implement, document, and test this function"
 diffE     = error "Implement, document, and test this function"
